e66935dbf450f6a7c454894decca2be6560ba9be
Patrick Oladimeji <github@dustygem.co.uk>
2017-05-09 10:17:43 +0100

    added patch fix for v1.3.0 component issues

--- packages/electrode-archetype-react-component/config/webpack/webpack.config.coverage.js
+++ packages/electrode-archetype-react-component/config/webpack/webpack.config.coverage.js
@@ -4,7 +4,7 @@
  * Webpack frontend test (w/ coverage) configuration.
  */
 var archDevRequire = require("electrode-archetype-react-component-dev/require");
-
+var Path = require("path");
 var testCfg = require("./webpack.config.test");
 var baseProfile = require("./profile.base");
 var testBaseProfile = require("./profile.base.test");
@@ -12,22 +12,22 @@ var testBaseProfile = require("./profile.base.test");
 var generateConfig = require("./util/generate-config");
 
 function makeConfig() {
-  const browserCoverageProfile = {
-    partials: {
-      "_coverage": {
-        order: 10100
-      }
-    }
+  const coverageProfile = {
+	partials: {
+	  "_coverage": {
+		order: 10100
+	  }
+	}
   };
 
   const options = {
-    profiles: {
-      "_base": baseProfile,
-      "_test-base": testBaseProfile,
-      "_coverage": coverageProfile
-    },
-    profileNames: ["_base", "_test-base", "_coverage"],
-    configFilename: Path.basename(__filename)
+	profiles: {
+	  "_base": baseProfile,
+	  "_test-base": testBaseProfile,
+	  "_coverage": coverageProfile
+	},
+	profileNames: ["_base", "_test-base", "_coverage"],
+	configFilename: Path.basename(__filename)
   };
 
   return generateConfig(options);
